d5ccc4e893ea881a39b7e40078a1d95f5f8cd172
mrq <maf.quintas@gmail.com>
2023-07-10 04:01:37 +0100

    .

--- index.js
+++ index.js
@@ -32,15 +32,15 @@ app.use("/api/users", usersRouter);
 
 
 // ---- 404
-app.use((req,res,next) => {
-    res.status(404).send.apply({msg:"No resource or page found."});
-})
+app.use((req, res, next) => {
+  res.status(404).send({ msg: "No resource or page found." });
+});
 
 // ---- Not treated
-app.use((err,req,res,next) => {
-    console.error(err)
-    res.status(500).send(err);
-})
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
+});
 
 const port = parseInt(process.env.port || '8080');
 app.listen(port,function() {
